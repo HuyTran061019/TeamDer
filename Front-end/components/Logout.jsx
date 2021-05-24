af5340afa9990e945a519d519067db0e1bf522ed
insectaerc <agilastic@gmail.com>
2021-05-25 04:00:59 +0700

    Update Logout button

--- Front-end/components/Logout.jsx
+++ Front-end/components/Logout.jsx
@@ -11,12 +11,13 @@ export default class Logout extends Component{
 
     render(){
         return(
-            <div className=" container ">
-                    <h1>You have been logged out!!</h1>
-                    <Link to="/" className="nav-link">
-                                    Login again
-                </Link>
-
+            <div className=" container text-center">
+                <h4>You have been logged out!</h4>
+                    <button className='btn btn-success'>
+                        <Link to="/" className="nav-link h5">
+                            Login again
+                        </Link>
+                    </button>
                 <div style={{  height: '500px' }}></div>
             </div>
         )
