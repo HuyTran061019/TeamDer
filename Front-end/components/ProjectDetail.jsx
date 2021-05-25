dc6a099d7d60f9c485aebc6f13eeaa1ab6d73000
insectaerc <agilastic@gmail.com>
2021-05-26 03:11:24 +0700

    Fix bugs
    Bug description: when there are more than 1 post/comment, if we edit the first post/comment, the last one will be updated instead

--- Front-end/components/ProjectDetail.jsx
+++ Front-end/components/ProjectDetail.jsx
@@ -131,8 +131,9 @@ export default class ProjectDetail extends React.Component {
         this.setState({ ownerId: this.state.check, postId: this.state.Setid, content: '', addNew: true })
     }
 
-    edit(content) {
+    edit(_id, content) {
         this.setState({ ownerId: this.state.check, postId: this.state.Setid, content: content, addNew: false })
+        this.updateCommentId(_id)
     }
     updateCommentId(_id){
         this.state.commentId = _id
@@ -166,13 +167,12 @@ export default class ProjectDetail extends React.Component {
                                                 {i.ownerId}
                                             </Link>
                                         </div>
-                                        {this.updateCommentId(i._id)}
                                         <div className="card-body py-0 font-weight-bold">
                                             {i.content}
                                             { i.ownerId == this.state.check?
                                             <div className='mt-3 text-center'>
                                                 <div className='d-inline'>
-                                                    <button className='btn btn-secondary font-weight-bold' data-toggle="modal" data-target="#myModal" onClick={this.edit.bind(this, i.content
+                                                    <button className='btn btn-secondary font-weight-bold' data-toggle="modal" data-target="#myModal" onClick={this.edit.bind(this, i._id, i.content
                                                     )}>
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill mr-1" viewBox="0 0 16 16">
                                                             <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
