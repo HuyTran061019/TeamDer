b0f91f77f5f3caf9060897668d9b6a84ad0ad07e
insectaerc <agilastic@gmail.com>
2021-05-27 05:54:59 +0700

    Update Project List UI

--- Front-end/components/ProjectList.jsx
+++ Front-end/components/ProjectList.jsx
@@ -77,9 +77,24 @@ export default class ProjectList extends React.Component {
                     <div className='card border-secondary mx-4 mb-3'>
                         {this.state.posts.filter(s => s.ownerId.toLowerCase().includes(this.state.keyword.toLowerCase()) && s.postStatus == 'Available' || s.postTitle.toLowerCase().includes(this.state.keyword.toLowerCase()) && s.postStatus == 'Available').map(filteredS =>
                             <div className='card my-3 mx-3 text-center text-md-start' key={filteredS._id}>
+                                
                                 <div className='card-header text-dark'>
-                                    <h5>{filteredS.postTitle}</h5>
+                                    <div className='row'>
+                                        <div className='col-md-10 d-flex align-items-center'>
+                                            <h5 className='my-auto'>{filteredS.postTitle}</h5>
+                                        </div>
+                                        <div className='col-md-2'>
+                                            <Link to={`/ProjectDetail/${filteredS._id}`}>
+                                                <button type="button" className="btn btn-info">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-info-lg" viewBox="0 0 16 16">
+                                                        <path d="m10.277 5.433-4.031.505-.145.67.794.145c.516.123.619.309.505.824L6.101 13.68c-.34 1.578.186 2.32 1.423 2.32.959 0 2.072-.443 2.577-1.052l.155-.732c-.35.31-.866.434-1.206.434-.485 0-.66-.34-.536-.939l1.763-8.278zm.122-3.673a1.76 1.76 0 1 1-3.52 0 1.76 1.76 0 0 1 3.52 0z"/>
+                                                    </svg>
+                                                </button>
+                                            </Link>
+                                        </div>
+                                    </div>
                                 </div>
+                                
                                 <div className='row card-body text-dark'>
                                     <div className='col-md-6'>
                                         <h5>Owner ID: {filteredS.ownerId}</h5>
@@ -89,14 +104,6 @@ export default class ProjectList extends React.Component {
                                     <div className='col-md-6'>
                                         <h5>Course Name: {filteredS.courseName}</h5>
                                         <h5>Requirement: {filteredS.requirement}</h5>
-                                        <h5>More Information: &nbsp;
-                                        <Link to={`/ProjectDetail/${filteredS._id}`}>
-                                            <button type="button" className="btn btn-success">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-info-lg" viewBox="0 0 16 16">
-                                                    <path d="m10.277 5.433-4.031.505-.145.67.794.145c.516.123.619.309.505.824L6.101 13.68c-.34 1.578.186 2.32 1.423 2.32.959 0 2.072-.443 2.577-1.052l.155-.732c-.35.31-.866.434-1.206.434-.485 0-.66-.34-.536-.939l1.763-8.278zm.122-3.673a1.76 1.76 0 1 1-3.52 0 1.76 1.76 0 0 1 3.52 0z"/>
-                                                </svg>
-                                            </button>
-                                        </Link></h5>
                                     </div>
                                 </div>
                             </div>
